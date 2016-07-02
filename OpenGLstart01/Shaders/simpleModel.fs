994092155a5b6e86325e0f62d2cf4ce2297d80e9
Przemyslaw Machura <przemekmachura94@gmail.com>
2016-07-02 19:18:11 +0200

    Dodanie komentarzy do oswietlenia phonga w fragment shaderze

--- OpenGLstart01/Shaders/simpleModel.fs
+++ OpenGLstart01/Shaders/simpleModel.fs
@@ -22,25 +22,28 @@ vec3 specularColor=vec3(1.0,1.0,1.0);
 
 
 void main(){
-	//Ambient
+	//Ambient - swiatlo otoczenia jako iloczyn globalnie ustalonego swiatla otoczenia z wspolczynnikiem konkretnego obiektu sceny
 	vec3 ambientIntense=ambientColor*objectAmbientFactor;
 	
-	//Difuse
-	vec3 distanceToLight= vec3(viewMatrix*vec4(lightPosition,1.0))-vertexToCamera;
-	vec3 lightDirection= normalize(distanceToLight);
-	float dotProduct=dot(lightDirection,normalToCamera);
-	dotProduct=max(dotProduct,0.0);
-	vec3 diffuseIntense=diffuseColor*objectDiffuseFactor*dotProduct;
+	//Difuse - swiato rozproszone 
+	vec3 distanceToLight= vec3(viewMatrix*vec4(lightPosition,1.0))-vertexToCamera; //roznica miedzy wektorem swiatla, a wektorem punktu w przestrzeni (po przeliczeniu co do pozycji kamery)
+	vec3 lightDirection= normalize(distanceToLight); // normalizacja wektora dlg 1, zachowanie kierunku
+	float dotProduct=dot(lightDirection,normalToCamera); //iloczyn skalarny - wartosc tego, czy zrodlo swiatla ze sceny dociera do punktu
+	dotProduct=max(dotProduct,0.0); // gdyby iloczyn skalarny byl ujemny- swiatlo nie dociera to ustawiamy na 0
+	vec3 diffuseIntense=diffuseColor*objectDiffuseFactor*dotProduct; // uzalezniamy intensywnosc swiatla rozproszenia od wartosci dla konkretnego modelu
 	
-	//Specular
-	vec3 reflection=reflect(-lightDirection,normalToCamera);
-	vec3 surfaceToCamera=normalize(-vertexToCamera);
-	float dotSpecular= dot(reflection,surfaceToCamera);
-	dotSpecular=max(dotSpecular,0.0);
-	float specularPower=10.0;
-	float specularFactor=pow(dotSpecular,specularPower);
-	vec3 specularIntense=specularColor* objectSpecularFactor*specularFactor;
+	//Specular lustrzane
+	vec3 reflection=reflect(-lightDirection,normalToCamera); // wektor odbicia kierunku swiatla do wektora normalnego pozycji kamery (kat padania = kat odbicia)
+	vec3 surfaceToCamera=normalize(-vertexToCamera); // znormalizowany wektor 
+	float dotSpecular= dot(reflection,surfaceToCamera); //iloczyn skalarny - wartosc tego, czy swiatlo odbite dociera do kamery
+	dotSpecular=max(dotSpecular,0.0); // gdyby swiatlo odbite nie doceiaralo do kamery (cosinus ujemny) ustawiamy wartosc 0
+	float specularPower=10.0; // wspolczynik mocy odbicia 
+	float specularFactor=pow(dotSpecular,specularPower); 
+	vec3 specularIntense=specularColor* objectSpecularFactor*specularFactor; //intensywnosc zalezna od konkretnego modelu sceny
 	
-	vec4 texel = texture(textureIndex, textureCoordinates);
+  //uzywana funkcja cosinus oddaje jakby efekt intensywnosci swiatla- im obiekt dalej od zrodla swiatla lub kierunek patrzenia mniej zgodny z kierunkiem padania swiatla
+  //tym szybciej swiatlo np. odbite jest mniej widoczne
+  
+	vec4 texel = texture(textureIndex, textureCoordinates);  
 	fragColour = vec4(ambientIntense+diffuseIntense+specularIntense,1.0)*texel;
 }
